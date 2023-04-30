0bc41aef73d8dddb048ad7c63200a8791e01c2aa
Ivanovich0705 <ivantenis1@icloud.com>
2023-04-30 11:09:46 -0500

    PROD_URL changed for tomcat ip

--- common/http.ts
+++ common/http.ts
@@ -1,6 +1,6 @@
 
 const LOCAL_URL = 'http://localhost:8080';
-const PROD_URL = 'https://lifetravel-backend-open-source.herokuapp.com';
+const PROD_URL = 'http://172.174.244.114:8080';
 const BASE_URL = PROD_URL;
 
 export default BASE_URL;
